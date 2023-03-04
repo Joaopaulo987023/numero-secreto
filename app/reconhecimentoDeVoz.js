c32455e3518ef9728a3d9a79715a882858168585
João Paulo <46024524+Joaopaulo987023@users.noreply.github.com>
2023-03-03 21:10:14 -0300

    finalizei projeto

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -9,15 +9,18 @@ recognition.start();
 //criando um evento para captura a voz e exibir no console
 recognition.addEventListener('result', (e)=>{
     const chute = e.results[0][0].transcript; //navegando pelo dom até encontrar o valor que foi dito
+    gameOver(chute);
     mostraChuteNaTela(chute)
     verificaSeOChutePossuiValorValido(chute);
+    
     
 })
 
 function mostraChuteNaTela(chute){
     elementoChute.innerHTML = `
-    <div>Você disse : </div>
+    <div>Voce disse : </div>
     <span class= 'box'>${chute}</span>` // criando divs para que apareça o valor que foi dito na tela.
     
 }
+if(chute)
 recognition.addEventListener('end',() => recognition.start())
--- app/validacao.js
+++ app/validacao.js
@@ -1,25 +1,42 @@
-function verificaSeOChutePossuiValorValido(chute){
-    const numero = +chute //transformando valor string em array
+
+function gameOver(chute){
+    
+    if(chute === 'game over'){
+        document.body.innerHTML = '<h2>GAME OVER</h2>'
+        document.body.style.backgroundColor = '#DF2E38';
+
+    }
+}
+    
+
+function verificaSeOChutePossuiValorValido(valorChute){
+    const numero = +valorChute //transformando valor string em array
     if (chuteForInvalido(numero)){ // verificando se o valor é um número invalido
-        elementoChute.innerHTML+=`<div>Valor inválido</div>`
+        elementoChute.innerHTML+=`<div>Valor invalido</div>`
         return
     }
+    
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)){
-        elementoChute.innerHTML+=`<div>Valor inválido, fale um número entre ${menorValor} e ${maiorValor}</div>`
+        elementoChute.innerHTML+=`<div>Valor invalido, fale um numero entre ${menorValor} e ${maiorValor}</div>`
         return
     }
     if(numero === numeroSecreto){
-        document.body.innerHTML=`<h2>Você acertou !! </h2>
-        <h3> O número secreto era ${numeroSecreto}</h3>
-        <button id ='jogar-novamente' class = 'btn-jogar' >Jogar novamente</button>`
+        
+        document.body.innerHTML=`<h2>Voce acertou !! </h2>
+        <h3> O numero secreto era ${numeroSecreto}</h3>
+        <button id ='jogar-novamente' class = 'btn-jogar' >JOGAR NOVAMENTE</button>`
+        document.body.style.backgroundColor = '#F5B971';
        
         
+         
     }
+   
     else if(numero>numeroSecreto){
-        elementoChute.innerHTML+=`<div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>`
+        elementoChute.innerHTML+=`<div>O numero secreto é menor <i class="fa-solid fa-arrow-down"></i></div>`
     }
+   
     else{
-        elementoChute.innerHTML+=`<div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>`
+        elementoChute.innerHTML+=`<div>O numero secreto é maior <i class="fa-solid fa-arrow-up"></i></div>`
     }
 }
 
